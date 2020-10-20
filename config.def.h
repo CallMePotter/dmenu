6873d804b1a249b5e5906170af6c7d775323852a
Dmitriy Potter <dmitriypottero@gmail.com>
2020-10-20 23:25:03 +0500

    font and colors

--- config.def.h
+++ config.def.h
@@ -6,13 +6,13 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"DejaVu Sans Mono:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#458588" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
