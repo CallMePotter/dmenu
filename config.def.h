ad4110c58d36e80bfb9fb4ba7a8de5391f17f0f1
potter <potter@art.art>
2024-04-13 20:25:25 +0500

    neon purple

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#458588" },
+	[SchemeNorm] = { "#eeeeee", "#2e3440" },
+	[SchemeSel] = { "#eeeeee", "#81a1c1" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
